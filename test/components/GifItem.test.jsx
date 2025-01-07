af26c72907ab5c5f7ad186117354ebcb239e772c
Daniel Carbajal <36678639+BeetBeatBit@users.noreply.github.com>
2025-01-06 18:20:21 -0600

    Testing P2

--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -1,12 +1,24 @@
-import { render } from "@testing-library/react"
-import { GifItem } from "../../src/components/GifItem"
+import { render, screen } from "@testing-library/react";
+import { GifItem } from "../../src/components/GifItem";
 
-describe('Pruebas en GifItem Component', () => {
-    const title = 'Saitama'
-    const url = 'prueba'
+describe("Testing GifItem Component", () => {
+  const title = "Saitama";
+  const url = "http://saitama.com/gift";
 
-    test('Match with Snapshot', () => {
-        const { container }= render( <GifItem title={title} url={url}/>)
-        expect(container).toMatchSnapshot();
-     })
-})
+  test("Match with Snapshot", () => {
+    const { container } = render(<GifItem title={title} url={url} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test("Must Show given URL and ALT ", () => {
+    render(<GifItem title={title} url={url} />);
+    //screen.debug();
+
+    //expect(screen.getByRole("img").src).toBe(url);
+    //expect(screen.getByRole("img").alt).toBe(title);
+
+    const { src, alt } = screen.getByRole("img");
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+});
